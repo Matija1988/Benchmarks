6d484e88c0bbbf7828bd251846578c35238aade7
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2024-05-06 10:50:50 +0200

    Benchmark testing String vs StringBuilder, Int Array vs Int List

---
+++ Benchmark/IntArrayVsIntList.cs
@@ -0,0 +1,52 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benchmark
+{
+    [MemoryDiagnoser]
+    public class IntArrayVsIntList
+    {
+        [Params(10, 50, 100)]
+        public int Count;
+
+        [Benchmark]
+        public int[] ArrayPerformance()
+        {
+            int[] items = new int[Count];
+            for(int i = 0; i < Count; i++)
+            {
+                items[i] = i;
+            }
+            return items;
+        }
+
+        [Benchmark]
+        public List<int> GenericListPerformanceWithCapacity()
+        {
+            List<int> items = new List<int>(Count);
+            for(int i = 0; i< Count; i++) 
+            { 
+            items.Add(i);
+            }
+            return items;
+        }
+        [Benchmark]
+        public List<int> GenericListPerformanceWithoutCapacity()
+        {
+            List<int> items = new List<int>();
+
+            for(int i = 0; i < Count;i++)
+            {
+                items.Add(i);
+            }
+
+            return items;
+
+        }
+
+    }
+}
---
+++ Benchmark/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Benchmark;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+using System.Text;
+
+
+var results = BenchmarkRunner.Run<IntArrayVsIntList>();
+
+
+[MemoryDiagnoser]
+public class Demo
+{
+    [Benchmark]
+    public string GetFullStringNormally()
+    {
+        string output = "";
+
+        for(int i = 0; i <100 ; i++) 
+        {
+            output += i;
+        }
+        return output;
+    }
+
+    [Benchmark]
+    public string GetFullStringWithStringBuilder() 
+    { 
+        StringBuilder output = new StringBuilder();
+
+        for(int i = 0; i < 100 ; i++) 
+        {
+            output.Append(i);
+            
+        }
+        return output.ToString();
+    }
+
+}
